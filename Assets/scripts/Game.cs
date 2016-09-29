0ff383f7b773e662225f1b657117a835afb094a7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-29 12:35:54 +0100

    Add camera follow function

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	private Camera mainCam;
+	[SerializeField]
+	private Kart kart;
+	[SerializeField]
+	private Vector3 cameraOffset;
+	private float forwardOffset = 1;
+
+	private void Start () {
+		mainCam = Camera.main;
+		cameraOffset = mainCam.transform.position - kart.transform.position;
+	}
+
+	private void Update () {
+		mainCam.transform.position = new Vector3(
+			kart.transform.position.x - kart.transform.forward.x * 2,
+			kart.transform.position.y + cameraOffset.y,
+			kart.transform.position.z - kart.transform.forward.z * 2
+		);
+		mainCam.transform.LookAt (kart.transform.position + kart.transform.forward * forwardOffset);
+	}
+}
