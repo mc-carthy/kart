4929e4bbba1ab784c0a08fd5fe075eaaf71fca78
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-29 12:18:07 +0100

    Add kart rotation and longitudintal motion

---
+++ Assets/scripts/Kart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kart : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float forwardSpeed = 10f;
+	private float rotateSpeed = 100f;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	private void Update () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		transform.RotateAround (transform.position, Vector3.up, h * rotateSpeed * Time.deltaTime);
+
+		float v = Input.GetAxisRaw ("Vertical");
+		rb.velocity =  transform.forward * v * forwardSpeed;
+	}
+}
